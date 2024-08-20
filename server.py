382d3330fa95c8470e6af6130bc013e89654e50c
Sani Omotola <noble.b.sage01@gmail.com>
2024-08-20 07:52:12 +0100

    first commit

---
+++ server.py
@@ -0,0 +1,145 @@
+import pyrebase
+from flask import Flask, abort, jsonify, render_template, request
+from flask_cors import CORS
+
+Config = {
+    "apiKey": "AIzaSyBy6ESU3Ash8nzRnM1jQobRuEdbZ8qp8Og",
+    "authDomain": "ecommerce-e6182.firebaseapp.com",
+    "databaseURL": "https://ecommerce-e6182-default-rtdb.firebaseio.com",
+    "projectId": "ecommerce-e6182",
+    "storageBucket": "ecommerce-e6182.appspot.com",
+    "messagingSenderId": "694607965954",
+    "appId": "1:694607965954:web:9f29f327203a39a6855c1b",
+  }
+
+firebase = pyrebase.initialize_app(Config)
+auth = firebase.auth()
+storage = firebase.storage()
+db = firebase.database()
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/products", methods=['GET'])
+def get_products():
+    try:
+        all_user = db.child("products").get()
+        products = all_user.val()
+        return jsonify(products)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+@app.route("/signup", methods=['POST'] )
+def signup():
+    ref = firebase.database().child("Users")
+    data = request.get_json()
+    
+    name = data.get('name')
+    email = data.get('email')
+    password = data.get('password')
+
+    user_data = {
+        "name": data.get("name"),
+        "email":data.get("price")
+        
+    }
+
+    if not name or not email or not password:
+        return jsonify({"error":"Name,Email and Password are required"}),400
+
+    try:
+        user = auth.create_user_with_email_and_password(email,password)
+
+        key = ref.child(name).set(user_data)
+        return jsonify({"message":"Succesfully created user"}),201
+
+        
+        
+    except Exception as e:
+        print(f"Error creating user: {str(e)}")
+        return jsonify({"error": "Falied to create user"}),500   
+    
+@app.route("/login",methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return jsonify({"error":"Email and Password are required"}),400
+
+    try:
+        login = auth.sign_in_with_email_and_password(email,password)
+        
+        return jsonify({"message":"logged in succesfully"}),201
+    except:
+        return jsonify({"message":"Invalid email or password"})
+
+@app.route("/Addproducts", methods=['POST'])
+def addProducts():
+    try:
+        ref = firebase.database().child("products")
+        data = request.get_json()
+
+        product_name = data.get("name")
+        product_price = data.get("price")
+        product_code = data.get("product_code")
+
+        product_data = {
+            "name": data.get("name"),
+            "price":data.get("price"),
+            "product_code":data.get("product_code")
+        }
+        key = ref.child(product_code).set(product_data)
+        return jsonify({"message":"Product added succesfully", "key":product_code})
+    except Exception as e:
+        print(f"Error adding product: {str(e)}")
+        return jsonify({"error":"Failed to add product"})
+    
+@app.route("/ProductPage/<product_key>", methods=['POST'])
+def Product(product_key):
+    try:
+        product_ref = db.child('products').child(product_key).get()
+        product_data = product_ref.val()
+
+        if product_data:
+            """ print(f"User Data: {user_doc.to_dict()}") """
+            return jsonify({"product":product_data})
+        else:
+            print({"Error":"No such user!"})
+            return None
+            
+    except Exception as e:
+        return jsonify({"Error":"Error occurred"})
+    
+
+
+
+@app.route("/AddtoCart/<product_key>", methods=['POST'])
+def AddtoCart(product_key):
+    try:
+        data = request.get_json()
+        user_id = data.get(user_id)
+
+        if not user_id:
+            return jsonify({"Error":"User ID is required"})
+        
+        product_ref = db.child('products').child(product_key).get()
+        product_data = product_ref.val()
+
+        if product_data:
+            cart_ref = db.child('users').child(user_id).child('cart').child(product_key)
+            cart_ref.set(product_data)
+            return jsonify({"message":"product added succesfully to cart","product":product_data})
+        else:
+            print({"Error":"No such product"})
+            
+            
+    except Exception as e:
+        return jsonify({"Error":"Error occurred","details": str(e)})
+    
+ 
+
+if __name__ == "__main__":
+    app.run(debug=True)
